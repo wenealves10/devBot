011ba1528b8accb001962422c52e45792107bd8b
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-05-04 22:14:09 -0300

    added file whatsapp ✅

--- src/whatsapp.ts
+++ src/whatsapp.ts
@@ -1,14 +1,26 @@
 import { Whatsapp } from "venom-bot";
-import { group } from "../tokens/tokens.json";
-import { Invite } from "./interfaces/invite";
+import { group2 } from "../tokens/tokens.json";
+import { UsersController } from "./controllers/UsersController";
+
+const usersController = new UsersController();
 
 async function main(whatsapp: Whatsapp) {
-  whatsapp.onMessage(async (message) => {
-    console.log(message.chatId);
+  whatsapp.onAddedToGroup(async (chatEvent) => {
+    await whatsapp.leaveGroup(String(chatEvent.id));
   });
-  whatsapp.onParticipantsChanged(group, async (event) => {
-    console.log(event);
-    await Invite(event, whatsapp);
+
+  whatsapp.onParticipantsChanged(group2, async (event) => {
+    await usersController.invite(event, whatsapp);
+  });
+
+  whatsapp.onAnyMessage(async (message) => {
+    if (!message.isGroupMsg) {
+      await whatsapp.blockContact(message.chatId);
+      return;
+    }
+
+    await usersController.register(message, whatsapp);
+    await usersController.perfil(message, whatsapp);
   });
 }
 
