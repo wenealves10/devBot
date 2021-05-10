d28cb7bdc1bd70909bbffb37923ea744bfdcd1b6
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-05-09 22:04:43 -0300

    Added new regex 💯

--- src/regex/index.ts
+++ src/regex/index.ts
@@ -1,6 +1,14 @@
-const checkCommandName = /^#name(\s+[\w\W]+)(\s+\d{2})(\s+[\w\W]+\s*)$/gi;
+const checkCommandName = /^#name(\s+)([A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ]+)(\s+)(\d{2})(\s+)([A-Za-z#+-./]+)$/gi;
+const checkCommandNumber = /^#add(\s+)(\d{12})$/gi;
 const getData = (data: string) => String(data.match(/\S+/gis)).split(",");
+const getNumber = (data: string) => data.match(/\d+/gis);
 const getStringClient = (client: any, search: string) =>
   String(client).split(search)[0];
 
-export { checkCommandName, getData, getStringClient };
+export {
+  checkCommandName,
+  checkCommandNumber,
+  getNumber,
+  getData,
+  getStringClient,
+};
